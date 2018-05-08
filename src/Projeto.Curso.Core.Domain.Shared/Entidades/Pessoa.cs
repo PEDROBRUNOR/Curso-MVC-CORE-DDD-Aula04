95efbccea02d659f21d247cd4b25658dae187b0a
Pedro Bruno <pedrobrunor@hotmail.com>
2018-05-07 23:16:51 -0300

    Aula 04

---
+++ src/Projeto.Curso.Core.Domain.Shared/Entidades/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using Projeto.Curso.Core.Domain.Shared.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Curso.Core.Domain.Shared.Entidades
+{
+    public abstract class Pessoa : EntidadeBase
+    {
+        public string Apelido { get; set; }
+        public string Nome { get; set; }
+        public CpfCnpjVO CPFCNPJ { get; set; }
+        public EmailVO Email { get; set; }
+        public EnderecoVO Endereco { get; set; }
+    }
+}
---
+++ src/Projeto.Curso.Core.Pedidos/Entidades/Fornecedores.cs
@@ -0,0 +1,12 @@
+﻿using Projeto.Curso.Core.Domain.Shared.Entidades;
+
+namespace Projeto.Curso.Core.Domain.Pedidos.Entidades
+{
+    public class Fornecedores : Pessoa
+    {
+        public override bool EstaConsistente()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
